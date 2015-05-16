dc0df07560dabb71782535423e4cc1d872f05f63
Juanma-4 <juanma.hernandez4@gmail.com>
2015-05-16 12:52:53 -0300

    Alta Prop no guarda bien algunos datos !

--- Inmo13/src/servicios/ServicioPropiedad.java
+++ Inmo13/src/servicios/ServicioPropiedad.java
@@ -52,7 +52,8 @@ public class ServicioPropiedad extends Application {
 													   propiedad.getCantBanio(),propiedad.getMetrosCuadrados(),
 													   propiedad.getParrillero(),propiedad.getGarage(),propiedad.getTipoPropiedad(),
 													   propiedad.getTipoEstado(),propiedad.getTipotransaccion(), 
-													   propiedad.getNumeroPuerta(),propiedad.getCalle() ,propiedad.getFid(),propiedad.getTipoMoneda(),propiedad.getPiso(),usuario);
+													   propiedad.getNumeroPuerta(),propiedad.getCalle() ,propiedad.getFid(),
+													   propiedad.getTipoMoneda(),propiedad.getPiso(),usuario);
 
 					booleanJSON = gson.toJson(creado);
 					
